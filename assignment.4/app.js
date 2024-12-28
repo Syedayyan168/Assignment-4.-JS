65efb29398e6c976e8f375856ca9f006eb443952
Syedayyan168 <syedayyanalimadni@gmail.com>
2024-12-28 04:11:02 -0800

    Add files via upload

---
+++ assignment.4/app.js
@@ -0,0 +1,176 @@
+//                     // ASSIGINMENT : NO : 4
+
+        // CHAPTER : NO : 14/16
+
+         //   QUESTION : NO : 1
+
+//    let studentNames = [];
+
+//   studentNames. push("AYYAN");  
+//   studentNames. push("AZZAN");  
+         
+//   console.log (studentNames);
+
+        //   QUESTION : NO : 2
+
+    //  let studentNames = {};
+
+    //  studentNames["sttudent1"] = "AYYAN";
+    //  studentNames["sttudent2"] = "AZZAN";
+
+    //  console.log(studentNames);
+
+    
+        //   QUESTION : NO : 3
+
+        // let stringsArray = ["AYYAN", "AZZAN", "AHMAD", "ALI"];
+
+        // console.log(stringsArray[0]);
+        // stringsArray.push("AYYAN");
+        // console.log(stringsArray);
+
+
+
+        //   QUESTION : NO : 4
+
+        //    let numberArray = [ "1", "2", "3", "4", "5"];
+
+        // console.log(numberArray[2]);
+        // numberArray.push("6");
+        // console.log(numberArray);
+
+
+        
+        //   QUESTION : NO : 5
+
+        // let booleanArray = [true, false, true, false];
+
+        // console.log(booleanArray[0]);
+        // booleanArray.push(true);
+        // console.log(booleanArray);
+
+        //   QUESTION : NO : 6
+
+        // let mixedArray = ["Alice", 25, true, { city: "New York" }, [1, 2, 3]];
+        
+
+        // console.log(mixedArray[0]);
+        // console.log(mixedArray[3].city); 
+        // mixedArray.push("New Element");
+        // console.log(mixedArray);
+ 
+
+         //   QUESTION : NO : 7
+
+        //  let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+
+        //  qualifications.forEach(function(qualification) {
+        //     console.log(qualification);
+        // });
+
+
+         //   QUESTION : NO : 8
+         //   QUESTION : NO : 9
+
+//         let studentNames = ["Alice", "Bob", "Charlie"];
+//         let studentScores = [450, 400, 475];
+
+
+//         for (let i = 0; i < studentNames.length; i++) {
+//             let studentName = studentNames[i];
+//             let studentScore = studentScores[i];
+//             let percentage = (studentScore / 500) * 100;
+        
+
+//         console.log(`Student: ${studentName}`);
+//     console.log(`Score: ${studentScore}/500`);
+//     console.log(`Percentage: ${percentage.toFixed(2)}%`);
+//     console.log('-----------------------------');
+// }
+
+
+      
+         //   QUESTION : NO : 10
+
+        //  let studentScores = [450, 300, 375, 500, 420];
+
+        //  studentScores.sort(function(a, b) {
+        //     return a - b;
+        // });
+
+
+        // console.log("Sorted student scores in ascending order:");
+        // console.log(studentScores);
+
+
+           //   QUESTION : NO : 11
+
+    //  let cities = ["New York", "London", "Paris", "Tokyo", "Sydney", "Berlin"];
+      
+
+    //  let selectedCities = cities.slice(0, 3);
+
+    //  console.log("Selected Cities:");
+    //  console.log(selectedCities);
+
+
+       //   QUESTION : NO : 12
+
+    //    var arr = ["This ", " is ", " my ", " cat"];
+
+    //    var result = arr.join("");
+
+    //    console.log(result);
+
+
+//   QUESTION : NO : 13
+
+    // let fifoQueue = [];
+
+
+    // fifoQueue.push("First");
+    // fifoQueue.push("Second");
+    // fifoQueue.push("Third");
+    // fifoQueue.push("Fourth");
+
+    // console.log(fifoQueue.shift());
+    // console.log(fifoQueue.shift());
+    // console.log(fifoQueue.shift());
+    // console.log(fifoQueue.shift());
+
+
+
+      // QUESTION : NO : 14
+
+
+    // let lifoStack = [];
+
+
+    // lifoStack.push("First");
+    // lifoStack.push("Second");
+    // lifoStack.push("Third");
+    // lifoStack.push("Fourth");
+
+    // console.log(lifoStack.pop());
+    // console.log(lifoStack.pop());
+    // console.log(lifoStack.pop());
+    // console.log(lifoStack.pop());
+
+     
+      // QUESTION : NO : 15
+    
+    //   let manufacturers = ["Apple", "Samsung", "Teckno", "Nokia", "Sony", "Haier"];
+
+
+    //   let selectMenu = document.createElement("select");
+
+    //   manufacturers.forEach(function(manufacturer) {
+    //     let option = document.createElement("option");
+    //     option.value = manufacturer; 
+    //     option.textContent = manufacturer;
+    //     selectMenu.appendChild(option);
+    //     });
+
+
+    //     document.body.appendChild(selectMenu);
